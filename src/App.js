3c0504a747d8b72a6a0972865c66aad9f105deee
Simran43P <159814727+Simran43P@users.noreply.github.com>
2025-09-02 15:26:47 +0530

    Update App.js with 5 page routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Login from "./Login"; 
+import Attendance from "./components/Attendance";
+import Expense from "./components/Expense";
+import FocusSession from "./components/FocusSession";
+import Library from "./components/Library";
+import Planner from "./components/Planner";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false); // login state
@@ -19,6 +24,35 @@ function App() {
             isLoggedIn ? <Dashboard /> : <Navigate to="/" replace />
           }
         />
+        {/* Library */}
+        <Route
+          path="/library"
+          element={isLoggedIn ? <Library /> : <Navigate to="/" replace />}
+        />
+
+        {/*Planner  */}
+        <Route 
+         path = "/planner"
+         element={isLoggedIn ? <Planner /> : <Navigate to = "/" replace />}
+         />
+
+         {/* Focus Session */}
+        <Route
+          path="/focus"
+          element={isLoggedIn ? <FocusSession /> : <Navigate to="/" replace />}
+        />
+
+        {/* Attendance */}
+        <Route
+          path="/attendance"
+          element={isLoggedIn ? <Attendance /> : <Navigate to="/" replace />}
+        />
+
+        {/* Expense */}
+        <Route
+          path="/expense"
+          element={isLoggedIn ? <Expense /> : <Navigate to="/" replace />}
+        />
 
         {/* Fallback */}
         <Route path="*" element={<Navigate to="/" replace />} />
@@ -28,4 +62,3 @@ function App() {
 }
 
 export default App;
-
